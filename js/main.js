257d1ab242e766e190a8d9fb1f2a7601d33ee0bb
Austin Song <austinhssong@gmail.com>
2024-02-05 17:01:27 -0500

    map, filter, reduce, foreach all completed

--- js/main.js
+++ js/main.js
@@ -5,7 +5,34 @@
 
 import {posts} from "posts.js"
 
-//forEach()
+//.forEach()
+//same as a for loop, iterates once
 posts.forEach(post => {
   console.log(post)
 });
+//returns each post once
+console.clear()
+
+
+//.filter()
+//used for boolean values, must pass through criteria to be returned
+const filteredPosts = posts.filter((post)=> post.userId === 1)
+console.log(filteredPosts)
+//returns all the posts with a userId of 1
+
+//.map()
+//applies function to ALL elements of array, returns new array
+const mappedPosts = posts.map((post)=>{
+  post.id * 10
+})
+console.log(mappedPosts)
+//multiplies all the post's ids by 10 and returns
+
+//.reduce()
+//applies function to return a single value
+//parameter is (accumulator, currentValue), accumulator is all outputs so far and currentValue is is the item being iterated over
+const reducedPostsValue = posts.reduce((sum, post) =>{
+  return sum + post
+})
+console.log(reducedPostsValue)
+//returns total sum of all posts
