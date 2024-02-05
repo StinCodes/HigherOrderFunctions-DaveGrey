15cd3ad0fba207a3555d2ecbe4720da2e78c27f3
Austin Song <austinhssong@gmail.com>
2024-02-05 16:38:53 -0500

    installed npm, running on localhost

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 //Higher Order Functions
   //Does 1 or both of the following...
     //-Takes 1 or more functions as an argument (parameter)
-    //Returns a function as its result
+    //Returns a function as the result
 
+import {posts} from "posts.js"
 
+//forEach()
+posts.forEach(post => {
+  console.log(post)
+});
